8c6e607f6badbf8262097dfa7625f53d955abd89
Si Yu Sun <siyu10.sun@gmail.com>
2026-04-24 18:13:45 +0200

    hotfix ws connection error

--- server/Sources/server/controllers/WebSocketControllert.swift
+++ server/Sources/server/controllers/WebSocketControllert.swift
@@ -14,15 +14,21 @@ class WebSocketController: RouteCollection {
 
   func boot(routes: any RoutesBuilder) throws {
     let protectedRoutes = routes.grouped(PlayerSessionGuardMiddleware())
+    let roomsService = self.roomsService
 
-    protectedRoutes.webSocket("games", ":code", "ws") { [weak self] req, ws in
-      try? await self?.connect(req: req, ws: ws)
+    protectedRoutes.webSocket("games", ":code", "ws") { req, ws in
+      do {
+        try await Self.connect(req: req, ws: ws, roomsService: roomsService)
+      } catch {
+        req.logger.report(error: error)
+        _ = try? await ws.close(code: .unexpectedServerError)
+      }
     }
   }
 }
 
 extension WebSocketController {
-  func connect(req: Request, ws: WebSocket) async throws {
+  static func connect(req: Request, ws: WebSocket, roomsService: RoomsService) async throws {
     guard let playerInfo = PlayerInfo(from: req.session) else {
       req.logger.info("Connection rejected: Missing or invalid session.")
       try await ws.send("Connection rejected: Missing or invalid session.")
@@ -34,8 +40,8 @@ extension WebSocketController {
 
     await roomsService.setWS(ws, for: playerInfo.id, in: playerInfo.roomCode)
 
-    ws.onClose.whenComplete { [weak self] _ in
-      Task { await self?.roomsService.setWS(nil, for: playerInfo.id, in: playerInfo.roomCode) }
+    ws.onClose.whenComplete { _ in
+      Task { await roomsService.setWS(nil, for: playerInfo.id, in: playerInfo.roomCode) }
     }
   }
 }
